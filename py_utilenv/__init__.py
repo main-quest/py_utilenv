fc426c697ee548b66019e8e3b3dbea970cacd8f3
Lucian Lazar <lucian@mainquest.ro>
2021-06-06 10:06:25 +0300

    first commig

---
+++ py_utilenv/__init__.py
@@ -0,0 +1,56 @@
+import os
+from pathlib import Path
+
+
+def add_or_update_env_var(key, val):
+    if os.name == 'nt':
+        os.system(f'SETX {key}="true"')
+        os.system(f'SET {key}="true"')
+    elif os.name == 'posix':
+        env_file_sys = '/etc/environment'
+        env_file_user = str(Path.home()) + '/.bashrc'
+
+        with open(env_file_sys, 'r') as f:
+            env_lines_sys = f.readlines()
+        with open(env_file_user, 'r') as f:
+            env_lines_user = f.readlines()
+
+        # env file format: VAR="<VALUE>"
+        prefix = f"{key}='"
+        line_to_set = f"{prefix}{val}'\r\n"
+        __add_or_update_var_on_line(env_lines_sys, prefix, line_to_set)
+
+        # .bashrc file format (just a bash script): export VAR="<VALUE>"
+        prefix = f"export {key}='"
+        line_to_set = f"{prefix}{val}'\r\n"
+        __add_or_update_var_on_line(env_lines_user, prefix, line_to_set)
+
+        with open(env_file_sys, 'w') as f:
+            f.writelines(env_lines_sys)
+        with open(env_file_user, 'w') as f:
+            f.writelines(env_lines_user)
+    else:
+        raise Exception('Case not handled: os.name=' + os.name)
+
+    # This applies the changes to the current session (so you won't need to log in and out)
+    # Commented: this was (almost silently) failing, because the command 'source' is a shell builtin, so we need
+    # another way of updating the current process' env
+    # os.system('source ' + ENV_FILE_SYS)
+    _environ = dict(os.environ.copy())
+    try:
+        _environ[key] = val
+    finally:
+        try:
+            os.environ.clear()
+        finally:
+            os.environ.update(_environ)
+
+
+def __add_or_update_var_on_line(lines: [str], line_prefix: str, line_to_set: str):
+    for i in range(0, len(lines)):
+        line = lines[i]
+        if line.startswith(line_prefix):
+            lines[i] = line_to_set
+            return
+
+    lines.append(line_to_set)
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='py_utilenv',
+    version='0.0.1',
+    description="Utils for environment setup (mostly environment vars). " +
+                "'Install by 'pip install --upgrade git+https://github.com/main-quest/py_utilenv.git#egg=py_utilenv'",
+    url='https://github.com/main-quest/py_utilenv.git',
+    author='The Fallen Games',
+    author_email='contact@thefallengames.com',
+    license='unlicense',
+    packages=find_packages(),
+    zip_safe=False
+)
